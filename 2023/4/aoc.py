4f6e9ca0a9cc388055e99a835d10fa7aa5981870
Gábor Vitrai <vitraig@gmail.com>
2023-12-04 22:51:33 +0100

    Day 4 Part 2

--- 2023/4/aoc.py
+++ 2023/4/aoc.py
@@ -13,7 +13,7 @@ def get_game_value(game, return_matches = False):
         if number in winning_numbers:
             game_value *= 2
             matches += 1
-    game_value = 0 if game_value == 1 else game_value / 2
+    game_value = 0 if game_value == 1 else int(game_value / 2)
     return matches if return_matches else game_value
 
 def task1(data):
@@ -23,7 +23,19 @@ def task1(data):
         solution += game_value
     return solution
 
+def task2(data):
+    game_counter = {i: 1 for i in range(0, len(data))}
+    for i in range(0, len(data)):
+        game_value = get_game_value(data[i], True)
+        if game_value > 0:
+            for j in range(0, game_counter[i]):
+                for game_id in range(i+1, i+1+game_value):
+                    game_counter[game_id] += 1
+    return sum(game_counter.values())
+
+
 if __name__ == "__main__":
     data = parse_data("input.txt")
     #print(data)
     print(task1(data))
+    print(task2(data))
