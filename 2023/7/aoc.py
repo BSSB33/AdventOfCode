236ebf80f881d7ffc8532a67284bac4229aee206
Gábor Vitrai <vitraig@gmail.com>
2023-12-08 15:29:00 +0100

    Day 7 Part 2

--- 2023/7/aoc.py
+++ 2023/7/aoc.py
@@ -6,7 +6,28 @@ def parse_data(path):
         data[i] = [[c for c in s[0]], int(s[1])]
     return data
 
-def get_hand_score(hand):
+def get_hand_score(hand, joker=False):
+    if joker and 'J' in hand:
+        # Joker acts like whatever card would make the hand the strongest type possible.
+        # e.g.: 2JJ4J -> 24444 -> 4 of a kind
+        #       2JJ4J -> 22244 -> 5 Full house
+        #       2JJ4J -> 22242 -> 4 of a kind
+        #       2JJ4J -> 2AA4A -> 3 of a kind
+        #       2JJ4J -> 2AA44 -> 2 pairs
+        #       2JJ4J -> 2AA42 -> 1 pair
+        
+        # finding the card that would make the hand the strongest
+        highest_score = 0
+        strongest_card = 'J'
+        for card in set(hand):
+            if card != 'J':
+                score = get_hand_score(hand + [card])
+                if score > highest_score:
+                    highest_score = score
+                    strongest_card = card
+        # replacing Js with the strongest card
+        hand = [card if card != 'J' else strongest_card for card in hand]
+
     # 5 of a kind
     if len(set(hand)) == 1:
         return 7
@@ -17,7 +38,7 @@ def get_hand_score(hand):
     elif any(hand.count(card) == 3 for card in set(hand)) and any(hand.count(card) == 2 for card in set(hand)):
         return 5
     # 3 of a kind
-    elif any(hand.count(card) == 3 for card in set(hand)) and len(set(hand)) == 3:
+    elif any(hand.count(card) == 3 for card in set(hand)) and not any(hand.count(card) == 2 for card in set(hand)):
         return 4
     # 2 pairs
     elif len([card for card in set(hand) if hand.count(card) == 2]) == 2:
@@ -30,9 +51,10 @@ def get_hand_score(hand):
         return 1
 
 
-def rank_hands(hands):
+def rank_hands(hands, joker=False):
     # symbols in descending order of value
     symbols = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
+    symbols = symbols[:3] + symbols[4:] + [symbols[3]] if joker else symbols
     hands = sorted(hands, key=lambda x: x[2], reverse=True)
     for i in range(len(hands)):
         for j in range(i+1, len(hands)):
@@ -47,10 +69,10 @@ def rank_hands(hands):
     return hands[::-1]
 
 
-def task1(hands):
+def task(hands, joker=False):
     for hand in hands:
-        hand.append(get_hand_score(hand[0]))
-    hands = rank_hands(hands)
+        hand.append(get_hand_score(hand[0], joker))
+    hands = rank_hands(hands, joker)
 
     total_winnings = 0
     for i in range(len(hands)):
@@ -58,8 +80,7 @@ def task1(hands):
     return total_winnings
 
 
+
 if __name__ == "__main__":
-    data = parse_data("input.txt")
-    #print(data)
-    print(task1(data))
-    #print(task2(data))
+    print(task(parse_data("input.txt")))
+    print(task(parse_data("input.txt"), joker=True))
